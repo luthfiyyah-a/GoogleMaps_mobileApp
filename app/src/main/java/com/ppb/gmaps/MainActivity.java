349457f65a0abd09b9dae4177f33c5cc387c1896
luthfiyyah hanifah amari <luthfiyyah.205025@gmail.com>
2023-11-15 11:05:00 +0700

    initial commit

---
+++ app/src/main/java/com/ppb/gmaps/MainActivity.java
@@ -0,0 +1,94 @@
+package com.ppb.gmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+// Implement OnMapReadyCallback.
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    GoogleMapOptions options = new GoogleMapOptions();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the layout file as the content view.
+        setContentView(R.layout.activity_main);
+
+        // Get a handle to the fragment and register the callback.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button go = (Button) findViewById(R.id.idGo);
+        go.setOnClickListener(op);
+    }
+
+    // Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng ITS = new LatLng(-7.28,112.79);
+        mMap.addMarker(new MarkerOptions().position(ITS).
+                title("Marker in ITS"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ITS,8));
+
+
+        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+                .compassEnabled(false)
+                .rotateGesturesEnabled(false)
+                .tiltGesturesEnabled(false);
+    }
+
+    View.OnClickListener op = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId()){
+                R.id.idGo:sembunyikanKeyBoard(view);
+                    gotoLokasi();
+                }
+            }
+    };
+
+    private void gotoLokasi()  {
+        EditText lat = (EditText) findViewById(R.id.idLokasiLat);
+        EditText lng = (EditText) findViewById(R.id.idLokasiLng);
+        EditText zoom = (EditText) findViewById(R.id.idZoom);
+        Double dbllat = Double.parseDouble(lat.getText().toString());
+        Double dbllng = Double.parseDouble(lng.getText().toString());
+        Float dblzoom = Float.parseFloat(zoom.getText().toString());
+        Toast.makeText(this,"Move to Lat:" +dbllat + " Long:" +dbllng,Toast.LENGTH_LONG).show();
+                    gotoPeta(dbllat,dbllng,dblzoom);
+    }
+
+
+    private void sembunyikanKeyBoard(View v){
+        InputMethodManager a = (InputMethodManager)
+            getSystemService(INPUT_METHOD_SERVICE);
+            a.hideSoftInputFromWindow(v.getWindowToken(),0);
+    }
+
+    private void gotoPeta(Double lat, Double lng, float z){
+        LatLng Lokasibaru = new LatLng(lat,lng);
+        mMap.addMarker(new MarkerOptions().
+            position(Lokasibaru).
+            title("Marker in  " +lat +":" +lng));
+        mMap.moveCamera(CameraUpdateFactory.
+            newLatLngZoom(Lokasibaru,z));
+    }
+}
